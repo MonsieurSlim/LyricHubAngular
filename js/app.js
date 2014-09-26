18a44a6764edc3a62c16d87abd0ef0530a8f32fe
MonsieurSlim <luccy559slim@gmail.com>
2014-09-26 11:24:33 +0100

    Updating

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('lyricHub', []);
 app.controller("lyricHubController", ['$scope','$http', function ($scope, $http) {
-	var startRequest = false;
+	$scope.startRequest = false;
 	var url = "https://lyrics.wikia.com/api.php";
 	var url1 = 'https://api.soundcloud.com/tracks.json';
 	var config = {
@@ -17,19 +17,18 @@ app.controller("lyricHubController", ['$scope','$http', function ($scope, $http)
 		};
 
 	$scope.getSongLyrics = function () {
-		startRequest = true;
+		$scope.startRequest = true;
 		$scope.getSongs();
 		config.params.song = $scope.songName.trim().toLowerCase();
 		config.params.artist = $scope.artistName.trim().toLowerCase();
 		$http.jsonp(url, config).success(function (response) {
 			console.log(response);
-			startRequest = false;
 			$scope.song = response;
 		});
 	};
 
 	$scope.getSongs = function () {
-		startRequest = true;
+		$scope.startRequest = true;
 		if ($scope.songName == "") {
 				config2.params.q = $scope.artistName.trim().toLowerCase();
 			}else {
@@ -37,11 +36,12 @@ app.controller("lyricHubController", ['$scope','$http', function ($scope, $http)
 			}
 		$http.jsonp(url1, config2).success(function (response) {
 			console.log(response);
-			startRequest = false;
+			$scope.listen = config2.params.client_id;
 			$scope.songs = response;
-			// var albumArt = response.artwork_url;
-			// if (albumArt == undefined) {
-			// 	albumArt = "img/albumArt.jpg";
+			// $scope.albumArt = $scope.songs.artwork_url;
+			// if ($scope.albumArt == null) {
+			// 	$scope.albumArt = "img/albumArt.jpg";
+			// }
 		});
 	};
 
