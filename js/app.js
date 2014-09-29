8fbe919c008e3e321675a34fdc84bd02fdc8f8e2
MonsieurSlim <luccy559slim@gmail.com>
2014-09-29 15:53:22 +0100

    Updates

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var app = angular.module('lyricHub', []);
 app.controller("lyricHubController", ['$scope','$http', function ($scope, $http) {
+	$scope.songName = "";
+	$scope.artistName = "";
 	var url = "https://lyrics.wikia.com/api.php";
 	var url1 = 'https://api.soundcloud.com/tracks.json';
 	var config = {
@@ -14,36 +16,53 @@ app.controller("lyricHubController", ['$scope','$http', function ($scope, $http)
 				callback: 'JSON_CALLBACK'
 				}
 		};
+	$scope.validate = function () {
+		console.log(songIn);
+		var songIn = $scope.songName.trim().toLowerCase();
+		var artistIn = $scope.artistName.trim().toLowerCase();
+		if((songIn.length < 1) && (artistIn.length < 1)) {
+			console.log("first condition passed");
+			alert("You must input one of either Song Name or Artist Name!");
+		}else {
+			$scope.getSongLyrics();
+			$scope.getSongs();
+		}
+	},
 
 	$scope.getSongLyrics = function () {
+		console.log("get lyrics is running");
 		$scope.startRequest = true;
-		$scope.getSongs();
-		config.params.song = $scope.songName.trim().toLowerCase();
-		config.params.artist = $scope.artistName.trim().toLowerCase();
-		$http.jsonp(url, config).success(function (response) {
-			console.log(response);
-			$scope.song = response;
-		});
-	};
+		var songIn = $scope.songName.trim().toLowerCase();
+		var artistIn = $scope.artistName.trim().toLowerCase();
+		config.params.song = songIn;
+		config.params.artist = artistIn;
+		if (songIn.length < 1 || artistIn.length < 1) {
+			$scope.errMessage = { text : "...Please put both the Artist and Song names to generate the lyrics. If you\'re uncertain, consult the search results on the right."};
+		}else {
+			$http.jsonp(url, config).success(function (response) {
+				console.log(response);
+				$scope.song = response;
+			});
+		}
+	},
 
 	$scope.getSongs = function () {
+		console.log("get songs is running");
 		$scope.startRequest = true;
-		if ($scope.songName == "") {
-				config2.params.q = $scope.artistName.trim().toLowerCase();
+		var songIn = $scope.songName.trim().toLowerCase();
+		var artistIn = $scope.artistName.trim().toLowerCase();
+		if (songIn.length < 1) {
+				config2.params.q = songIn;
 			}else {
-				config2.params.q = $scope.songName.trim().toLowerCase();
+				config2.params.q = artistIn;
 			}
 		$http.jsonp(url1, config2).success(function (response) {
 			console.log(response);
 			$scope.listen = config2.params.client_id;
 			$scope.songs = response;
-			// $scope.albumArt = $scope.songs.artwork_url;
-			// if ($scope.albumArt == null) {
-			// 	$scope.albumArt = "img/albumArt.jpg";
-			// }
 		});
 
-	};
+	},
 
 	$scope.albumArt = function (e) {
 		if(e == null) {
@@ -51,7 +70,12 @@ app.controller("lyricHubController", ['$scope','$http', function ($scope, $http)
 		}else {
 			return e;
 		}
-	};
+	}
+
+	// $scope.playSong = function () {
+	// 	$scope.songPlay.play();
+	// 	return false;
+	// };
 
 	
 }]);
